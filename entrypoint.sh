1b45abbdc381a97c7c44a7b6a3cf650f317a9fe6
Sean Redmond <sean.redmond1@gmail.com>
2020-06-01 09:15:15 +0100

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,6 @@ set -e
 if [ $(head -c 3 /sys/hypervisor/uuid) == "ec2" ]; then
   echo "We are running on EC2 so lets call secrets manager"
   secretid="${AppName}-${AppENV}"
-  echo "The Value of the secretid is ${secretid}"
-  echo aws secretsmanager get-secret-value --region eu-west-1 --secret-id "${secretid}" --query SecretString --output text
-  aws --version
   values=$(aws secretsmanager get-secret-value --region eu-west-1 --secret-id "${secretid}" --query SecretString --output text)
   for s in $(echo $values | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do export $s; done
 else
